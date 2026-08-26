938c68e3324ccdf5706d9f86b1c62338701a1852
yuuiioo0802 <yuuiioo0802@gmail.com>
2026-08-27 05:18:15 +0800

    Deploy to GitHub Pages

---
+++ _next/static/E6UGxzr-XsiXi807pvLsW/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/huangyuyu-jobwant-fortnight//_next/:path+",
+        "destination": "/huangyuyu-jobwant-fortnight/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
